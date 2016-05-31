16c3f08530b701c7a371db37ea3d65d59f9ab130
Mario <mariob.c@hotmail.com>
2016-05-30 19:45:37 -0500

    cambios google y agrego sublime 3

--- install_google-chrome.sh
+++ install_google-chrome.sh
@@ -1,12 +1,6 @@
-#!/bin/bash
-
-# MBC 5 feb 2016
-# Ejecutar terminal (Ctrl+Alt+T)
-# Ir al directorio donde está guardado este archivo (Ej: cd ~/Desktop) (~ manda a home)
-# Cambiarle los permisos al archivo para poder ejecutar (sudo chmod 777 install_google-chrome.sh)
-# Ejecutar archivo (./install_google-chrome.sh)
-
-sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo apt-get update
-sudo apt-get install -y google-chrome-stable
+sudo apt-get install libgconf2-4 libnss3-1d libxss1
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i --force-depends google-chrome-stable_current_amd64.deb
+rm google-chrome-stable_current_amd64.deb
+sudo apt-get -f install
