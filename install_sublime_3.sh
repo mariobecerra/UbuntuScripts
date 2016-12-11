38f84e3bd95e95020c8f616af14e2f0a9e2e4167
Mario Becerra <mbecerracontreras@gmail.com>
2016-12-10 23:10:30 -0600

    Update install_sublime_3.sh

--- install_sublime_3.sh
+++ install_sublime_3.sh
@@ -2,3 +2,6 @@ sudo add-apt-repository ppa:webupd8team/sublime-text-3
 sudo apt-get update
 sudo apt-get install sublime-text-installer
 
+# wget https://download.sublimetext.com/sublime-text_build-3126_amd64.deb
+# sudo dpkg -i sublime-text_build-3126_amd64.deb
+# sudo apt-get install -f
